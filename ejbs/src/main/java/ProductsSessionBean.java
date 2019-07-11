215458b8e98a8b93dae13bb24a04526ccba90b44
Muhamed Krlić <1l0oky1@gmail.com>
2019-07-11 21:25:03 +0200

    persistence.xml config

--- ejbs/src/main/java/ProductsSessionBean.java
+++ ejbs/src/main/java/ProductsSessionBean.java
@@ -1,20 +1,20 @@
 import models.ProductsEntity;
 
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.List;
 
+@Stateless(name = "ProductsSessionEJB")
 public class ProductsSessionBean implements ProductSessionEJBRemote{
-
+    public ProductsSessionBean() {
+    }
     private EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpa-PU");
     private EntityManager em = factory.createEntityManager();
 
     @Override
     public List<ProductsEntity> findAll() {
-        EntityTransaction tx = em.getTransaction();
-//        tx.begin();
         List<ProductsEntity> result = em.createNamedQuery("products.findAll").getResultList();
         return result;
     }
--- primary-source/src/main/java/ProductSessionEJBRemote.java
+++ primary-source/src/main/java/ProductSessionEJBRemote.java
@@ -1,7 +1,9 @@
 import models.ProductsEntity;
 
+import javax.ejb.Local;
 import java.util.List;
 
+@Local
 public interface ProductSessionEJBRemote {
     List<ProductsEntity> findAll();
 }
